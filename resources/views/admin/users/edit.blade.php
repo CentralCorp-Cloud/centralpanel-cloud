b3ed4ff2760e1776ca066585f6f0b9c0d0110440
Romain <50055727+Riptiaz@users.noreply.github.com>
2026-02-20 21:44:05 +0100

    Update

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -3,59 +3,66 @@
 @section('title', __('messages.users.edit_user'))
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">{{ __('messages.users.edit_user') }}</h3>
-                </div>
-                <div class="card-body">
-                    @if(session('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
-                        </div>
-                    @endif
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ __('messages.users.edit_user') }}</h3>
+                    </div>
+                    <div class="card-body">
+                        @if(session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
 
-                    <form action="{{ route('admin.users.update', $user->id) }}" method="POST">
-                        @csrf
-                        @method('PUT')
-                        
-                        <div class="form-group">
-                            <label for="name">{{ __('messages.users.name') }}</label>
-                            <input type="text" class="form-control @error('name') is-invalid @enderror" 
-                                   id="name" name="name" value="{{ old('name', $user->name) }}" required>
-                            @error('name')
-                                <span class="invalid-feedback">{{ $message }}</span>
-                            @enderror
-                        </div>
+                        <form action="{{ route('admin.users.update', $user->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
 
-                        <div class="form-group">
-                            <label for="email">{{ __('messages.users.email') }}</label>
-                            <input type="email" class="form-control @error('email') is-invalid @enderror" 
-                                   id="email" name="email" value="{{ old('email', $user->email) }}" required>
-                            @error('email')
-                                <span class="invalid-feedback">{{ $message }}</span>
-                            @enderror
-                        </div>
+                            <div class="form-group">
+                                <label for="name">{{ __('messages.users.name') }}</label>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                                    name="name" value="{{ old('name', $user->name) }}" required>
+                                @error('name')
+                                    <span class="invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
 
-                        <div class="form-group">
-                            <label for="password">{{ __('messages.users.new_password') }}</label>
-                            <input type="password" class="form-control @error('password') is-invalid @enderror" 
-                                   id="password" name="password">
-                            @error('password')
-                                <span class="invalid-feedback">{{ $message }}</span>
-                            @enderror
-                        </div>
+                            <div class="form-group">
+                                <label for="email">{{ __('messages.users.email') }}</label>
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
+                                    name="email" value="{{ old('email', $user->email) }}" required>
+                                @error('email')
+                                    <span class="invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
 
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">{{ __('messages.common.update') }}</button>
-                            <a href="{{ route('admin.users') }}" class="btn btn-secondary">{{ __('messages.common.back') }}</a>
-                        </div>
-                    </form>
+                            <div class="form-group">
+                                <label for="password">{{ __('messages.users.new_password') }}</label>
+                                <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                    id="password" name="password" placeholder="Laissez vide pour ne pas modifier">
+                                @error('password')
+                                    <span class="invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password_confirmation">{{ __('messages.users.password_confirm') }}</label>
+                                <input type="password" class="form-control" id="password_confirmation"
+                                    name="password_confirmation">
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">{{ __('messages.common.update') }}</button>
+                                <a href="{{ route('admin.users') }}"
+                                    class="btn btn-secondary">{{ __('messages.common.back') }}</a>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
